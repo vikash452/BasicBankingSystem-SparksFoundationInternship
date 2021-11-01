9e796546aa0be37384eae94212e4ad00f30f318d
vikash452 <vikashp0901@gmail.com>
2021-11-02 00:28:20 +0530

    server setup done and basic code started

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+require('dotenv').config
+
+mongoose.connect('mongodb://localhost:27017/BankingSystem',{useNewUrlParser:true,useUnifiedTopology:true});
+
+mongoose.connection.on('connected',()=>{
+    console.log('connected to database')
+})
+mongoose.connection.on('error',()=>{
+    console.log('failed to connect to database')
+})
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + '/public/html/home.html')
+})
+
+app.listen(PORT,()=>{
+    console.log(`server running on port ${PORT}`)
+})
